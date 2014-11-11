7c46e02d91c009c2129e3e10f3649f103d4c937e
Ryan Vice <Ryan@ViceSoftware.com>
2014-11-11 13:53:36 -0600

    First commit

---
+++ TestingCodingExercise/FizzTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestingCodingExercise
+{
+    [TestFixture]
+    class FizzTests
+    {
+        [Test]
+        public void Given0To4ShouldWriteFizzFor0And3()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestingCodingExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestingCodingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoFizz(Enumerable.Range(0, 100));
+        }
+
+        private static void DoFizz(IEnumerable<int> range)
+        {
+            foreach (var i in range)
+            {
+                if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
